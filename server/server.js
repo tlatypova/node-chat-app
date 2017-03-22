896b5cab934a2c32d2cd13a7403599559604484e
Tse <verbena70@comcast.net>
2017-03-22 16:27:49 -0400

    Update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,14 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
+
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
-
-module.exports = { app };
-
