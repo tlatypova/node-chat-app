7a04432bb868ff7f575bf0db6c797f65c5b2c0a5
Tse <verbena70@comcast.net>
2017-03-23 16:50:15 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -27,13 +27,15 @@ socket.on('newLocationMessage', function(message) {
 
 });
 
+var messageTextbox = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -43,12 +45,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text("Send location");
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text("Send location");
     alert('Unable to fetch location');
   });
 });
